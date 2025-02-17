8b5b15c756b4a906af4486a48c1cdd917f8c08d7
yangjiangping <yangjiangping@sinosoft.com.cn>
2025-02-17 11:58:12 +0800

    remove test

--- src/test/java/cn/atsuc/test/AtsucTestApplicationTests.java
+++ src/test/java/cn/atsuc/test/AtsucTestApplicationTests.java
@@ -1,20 +1,9 @@
 package cn.atsuc.test;
 
-import cn.atsuc.test.entity.ValueEntity;
-import jakarta.annotation.Resource;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AtsucTestApplicationTests {
 
-    @Resource
-    private ValueEntity valueEntity;
-
-    @Test
-    void contextLoads() {
-
-        System.out.println(valueEntity.getTomcatMe());
-    }
 
 }
